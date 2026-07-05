e820883a6711170246b15d3a812d3c4119afc7a9
jncldrn64 <juan.calderon64@correo.tdea.edu.co>
2026-07-05 05:41:58 +0000

    Delete backend directory

--- backend/src/routes/CategoriaRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { obtenerCategorias } from '../controller/CategoriaController.js';
-
-const router = express.Router();
-
-// Esta ruta es pública, cualquiera puede ver los filtros del catálogo
-router.get('/categorias', obtenerCategorias);
-
-export default router;
